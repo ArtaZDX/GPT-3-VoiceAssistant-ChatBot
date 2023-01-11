12543858e8d7cbcc7d214996d4b3f92c252e1d8a
ArtaZDX <arta.moghaddasi@gmail.com>
2023-01-11 16:18:35 +0000

    testing..

---
+++ main.py
@@ -0,0 +1,47 @@
+import openai
+import pyttsx3
+import speech_recognition as sr
+# Importing your special OpenAI APIKEY from api_key.py
+from api_key import API_KEY
+
+
+openai.api_key = API_KEY
+# Initializing engine
+engine = pyttsx3.init()
+
+r = sr.Recognizer()
+# Finding microphone
+mic = sr.Microphone(device_index=1)
+
+
+conversation = ""
+# Put your username here
+user_name = "Your username"
+# Put Bot's name here
+bot_name = "Bot's name"
+
+while True:
+    with mic as source:
+        print("\nlistening...")
+        r.adjust_for_ambient_noise(source, duration=0.2)
+        audio = r.listen(source)
+    print("no longer listening.\n")
+
+    try:
+        user_input = r.recognize_google(audio)
+    except:
+        continue
+
+    prompt = user_name + ": " + user_input + "\n" + bot_name+ ": "
+
+    conversation += prompt  # allows for context
+
+    # fetch response from open AI api
+    response = openai.Completion.create(engine='text-davinci-003', prompt=conversation, max_tokens=100)
+    response_str = response["choices"][0]["text"].replace("\n", "")
+    response_str = response_str.split(user_name + ": ", 1)[0].split(bot_name + ": ", 1)[0]
+    conversation += response_str + "\n"
+    print(response_str)
+
+    engine.say(response_str)
+    engine.runAndWait()         
